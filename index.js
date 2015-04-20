1b60b351ca8c827c84da3d3c4d5ddbf1a3b7eed5
Estelle DeBlois <edeblois@gmail.com>
2015-04-20 13:38:42 -0400

    WIP

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var JSCSFilter = require('broccoli-jscs');
+
 module.exports = {
-  name: 'ember-suave'
+  name: 'ember-suave',
+
+  lintTree: function(type, tree) {
+    var jscsOptions = this.app.options.jscsOptions || {};
+    jscsOptions.configPath = path.join(__dirname, 'lib', 'jscsrc.json');
+
+    return new JSCSFilter(tree, jscsOptions);
+  }
 };
---
+++ node_tests/rules-test.js
@@ -0,0 +1,75 @@
+var fs = require('fs');
+var path = require('path');
+var broccoli = require('broccoli');
+var walkSync = require('walk-sync');
+var expect = require('chai').expect;
+var suaveLintTree = require('../index').lintTree;
+
+describe('rules tests', function() {
+  var fixturePath = path.join(__dirname, 'fixtures');
+  var builder, originLog;
+
+  beforeEach(function() {
+    this.lintTree = suaveLintTree;
+    this.app = { options: {} };
+
+    originLog = console.log;
+    console.log = function(contents) {
+      if (!/at .+ :/.test(contents)) {
+        originLog.apply(console, arguments);
+      }
+    };
+  });
+
+  afterEach(function() {
+    console.log = originLog;
+
+    if (builder) {
+      return builder.cleanup();
+    }
+  });
+
+  var rules = fs.readdirSync(fixturePath);
+
+  rules.forEach(function(dir) {
+    describe(dir, function() {
+      it('good files should pass', function() {
+        var rulePath = path.join(fixturePath, dir, 'good');
+
+        return checkJSCSRules(this.lintTree('test', rulePath), function(contents) {
+          expect(contents).to.include('ok(true');
+          expect(contents).to.not.include('ok(false');
+        });
+      });
+
+      it('bad files should fail', function() {
+        var rulePath = path.join(fixturePath, dir, 'bad');
+
+        return checkJSCSRules(this.lintTree('test', rulePath), function(contents) {
+          expect(contents).to.not.include('ok(true');
+          expect(contents).to.include('ok(false');
+        });
+      });
+    });
+  });
+
+  function checkJSCSRules(tree, callback) {
+    var builder = new broccoli.Builder(tree);
+
+    return builder.build()
+      .then(function(result) {
+        var outputPath = result.directory;
+
+        var files = walkSync(outputPath)
+          .filter(function(file) {
+            return /jscs-test.js$/.test(file);
+          });
+
+        files.forEach(function(file) {
+          var contents = fs.readFileSync(path.join(outputPath, file), { encoding: 'utf8' });
+
+          callback(contents);
+        });
+      });
+  }
+});
