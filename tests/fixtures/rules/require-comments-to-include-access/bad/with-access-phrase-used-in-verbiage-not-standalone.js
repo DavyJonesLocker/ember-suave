06c2edf6aad24f1c2c8f1ebf7f14a8968960b43e
Robert Jackson <robert.w.jackson@me.com>
2015-06-06 12:45:03 -0700

    Require @private/@public in all block comments.
    Enforce all block comments to include YUIDoc style `@private` or
    `@public` access declaration.
    
    ```
    // good
    
    /**
    
     This thing is private.
    
     @private
    */
    export function somePrivateThing() {}
    
    /**
    
     This thing is private.
    
     @public
    */
    export function somePublicThing() {}
    
    // bad
    
    /**
      Something without access documented.
    */
    export function someNonDeterminedThing() {}
    
    /**
      This thing has @private and @public in the text description but not
      as an actual YUIDoc attribute.
    */
    export function ambiguousThing() {}
    ```

---
+++ tests/fixtures/rules/require-comments-to-include-access/bad/with-access-phrase-used-in-verbiage-not-standalone.js
@@ -0,0 +1,5 @@
+/**
+  This thing has @private and @public in the text description but not
+  as an actual YUIDoc attribute.
+*/
+export function ambiguousThing() {}
---
+++ tests/fixtures/rules/require-comments-to-include-access/good/example.js
@@ -0,0 +1,15 @@
+/**
+
+ This thing is private.
+
+ @private
+ */
+export function somePrivateThing() {}
+
+/**
+
+ This thing is public.
+
+ @public
+ */
+export function somePublicThing() {}
