ea6b8a2707afdad8f13ba84a80f1f00685a55b2c
Estelle DeBlois <edeblois@gmail.com>
2015-09-09 23:31:41 -0400

    Use ember-cli's UI instance instead of `console.log`

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 /* jshint node: true */
 'use strict';
+
 var JSCSFilter = require('broccoli-jscs');
 var jscsrcBuilder = require('./lib/jscsrc-builder');
 
 module.exports = {
   name: 'ember-suave',
 
-  lintTree: function (type, tree) {
+  lintTree: function(type, tree) {
+    var ui = this.ui;
+
     var jscsOptions = this.app.options.jscsOptions || {};
     jscsOptions.configPath = jscsrcBuilder(this.project);
-    return new JSCSFilter(tree, jscsOptions);
+
+    var jscsFilter = new JSCSFilter(tree, jscsOptions);
+
+    jscsFilter.logError = function(errorText) {
+      ui.writeLine(errorText);
+    };
+
+    return jscsFilter;
   },
 
   included: function(app) {
     this._super.included.apply(this, arguments);
+
     if (app.tests) {
       app.import('vendor/ember-suave/qunit-configuration.js', { type: 'test' });
       app.import('vendor/ember-suave/test-loader.js', { type: 'test' });
     }
   }
-
 };
